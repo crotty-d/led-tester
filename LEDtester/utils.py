ef3af6e906f8ea9d47055f91bbc8337ff9ea9e87
crotty-d <darragh@darraghcrotty.com>
2018-03-01 22:40:06 +0000

    initial utils.py

---
+++ LEDtester/utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+def parseFile(input):
+
+    if input.startswith('http'):
+        # use requests
+        return None, None
+    else:
+        # read from disk
+        # haven't written the code yet...
+        return None, None
+    return
+
+if __name__ == '__main__':
+    import sys
+    sys.exit()
