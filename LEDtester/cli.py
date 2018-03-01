f1a25405dc895a60a0c81de45d80dba105e2c6fb
crotty-d <darragh@darraghcrotty.com>
2018-03-01 21:25:28 +0000

    initial main function

--- LEDtester/cli.py
+++ LEDtester/cli.py
@@ -3,13 +3,22 @@
 """Console script for LEDtester."""
 
 import click
-
+click.disable_unicode_literals_warning = True
 
 @click.command()
 @click.option("--input", default=None, help="input URI (file or URL)")
 def main(input=None):
     """Console script for LEDtester."""
     print('input', input)
+    N, instructions = parseFile(input)
+
+    ledTester = LEDTester(N)
+
+    for instruction in instructions:
+        ledTester.apply(instruction)
+
+    print('Number occupied: ', ledTester.countOccupied()) 
+        
     return 0
 
 
